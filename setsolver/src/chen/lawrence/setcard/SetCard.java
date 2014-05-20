6f9d9e11a0a7e2800df58520179b0725ecc3dca2
Lawrence Chen <lawrenceochen@gmail.com>
2014-05-20 09:25:11 -0700

    added enum types for Card

--- setsolver/src/chen/lawrence/setcard/SetCard.java
+++ setsolver/src/chen/lawrence/setcard/SetCard.java
@@ -4,16 +4,28 @@
 package chen.lawrence.setcard;
 
 /**
+ * Representation of a card used in the card game Set.
+ * 
  * @author Lawrence
  *
  */
+
+
 public class SetCard {
+	
+	//Defining properties of actual Set cards
+	public enum Shape {SQUIGGLE, DIAMOND, CAPSULE};
+	public enum Number {ONE, TWO, THREE};
+	public enum Shading {OPEN, SHADED, FILLED};
+	public enum Color {RED, GREEN, PURPLE};
 
 	/**
+	 * Creates a card with randomly-generated properties.
 	 * 
 	 */
 	public SetCard() {
 		// TODO Auto-generated constructor stub
+		Math.random()
 	}
 
 }
