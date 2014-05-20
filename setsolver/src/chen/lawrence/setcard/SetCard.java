1e868b6769530fbff61478491aa8939bbe41838d
Lawrence Chen <lawrenceochen@gmail.com>
2014-05-20 09:04:18 -0700

    added setcard package

---
+++ setsolver/src/chen/lawrence/setcard/SetCard.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package chen.lawrence.setcard;
+
+/**
+ * @author Lawrence
+ *
+ */
+public class SetCard {
+
+	/**
+	 * 
+	 */
+	public SetCard() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
