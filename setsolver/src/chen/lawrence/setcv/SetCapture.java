2f03fef17700846086f31b639066461775c9f995
Lawrence Chen <lawrenceochen@gmail.com>
2014-06-04 20:06:07 -0700

    fixed issue where the FrameGrabber thread would not terminate

--- setsolver/src/chen/lawrence/setcv/SetCapture.java
+++ setsolver/src/chen/lawrence/setcv/SetCapture.java
@@ -3,6 +3,11 @@ package chen.lawrence.setcv;
 import org.bytedeco.javacpp.opencv_core.*;
 import org.bytedeco.javacv.*;
 
+import java.awt.Frame;
+import java.awt.event.*;
+
+import javax.swing.*;
+
 /**
  * Handles the webcam feed for SetSolver.
  * 
@@ -10,43 +15,111 @@ import org.bytedeco.javacv.*;
  *
  */
 public class SetCapture implements Runnable{
-	
+	//TODO documentation
 	SetSegmentation segInstance = new SetSegmentation();
 	CanvasFrame canvasFrame = new CanvasFrame("SetSolver");
+	FrameGrabber grabber;
 	int cameraID = 0;
 	
 	public SetCapture() {
-		canvasFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		setupClose();
 	}
 	
 	public SetCapture(int camera) {
-		canvasFrame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+		setupClose();
 		cameraID = camera;
 	}
 	
 	@Override
 	public void run() {
-		FrameGrabber grabber = new OpenCVFrameGrabber(cameraID);
+		grabber = new OpenCVFrameGrabber(cameraID);
 		try {
 			grabber.start();
 			IplImage img;
-			while(true) {
+			while(canvasFrame.isActive() && canvasFrame != null) {
 				img = grabber.grab();
+				canvasFrame.setCanvasSize(grabber.getImageWidth(), grabber.getImageHeight()); 
 				if (img != null) {
 					segInstance.setImage(img);
 					canvasFrame.showImage(segInstance.call());
 				}
+
 			}
+			System.out.println("qwop");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
+	private void setupClose() {
+		canvasFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		canvasFrame.addWindowListener(new GrabberListener());
+	}
+	
     @Override
     public String toString(){
         return new Integer(this.cameraID).toString();
     }
+    
+    /**
+     * Action listener that handles window closing, thread
+     * safety be damned.
+     * 
+     * @author Lawrence
+     *
+     */
+    private class GrabberListener implements WindowListener {
+    	
+    	@Override
+    	public void windowClosing(WindowEvent arg0) {
+    		if (grabber != null) {
+    			try {
+    				// Grabber must *always* be released first, otherwise
+    				// SetRun will never terminate
+					grabber.release();
+					canvasFrame.dispose();
+				} catch (Exception exc) {
+					// TODO Auto-generated catch block
+					exc.printStackTrace();
+				}
+    		}
+    	}
 
+		@Override
+		public void windowActivated(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
 
+		@Override
+		public void windowClosed(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowIconified(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowOpened(WindowEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+    }
 }
